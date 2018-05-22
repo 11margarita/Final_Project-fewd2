fbea2328f7fa3db64d0cbd046bd47d9d4b9f6644
11margarita <36007397+11margarita@users.noreply.github.com>
2018-05-22 00:15:21 -0700

    Add files via upload

---
+++ projectNEW/scripts/myscripts.js
@@ -0,0 +1,20 @@
+ $(document).ready(function(){
+    $('.bxslider').bxSlider({
+        captions: true,
+        auto: true,
+        mode: 'fade',
+         autoControls: true,
+  stopAutoOnClick: true,
+  pager: true,
+  slideWidth: 600,
+    });
+  });                                                                                                     /*                                                              
+$('.bxslider').bxSlider({
+     mode: 'fade',
+    captions: true,
+  auto: true,
+  autoControls: true,
+  stopAutoOnClick: true,
+  pager: true,
+  slideWidth: 600
+});
